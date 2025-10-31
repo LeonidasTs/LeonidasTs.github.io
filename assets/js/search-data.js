6be5ce8d6dfed0d6a295df74b792a5c6bab8559f
LeonidasTs <132682883+LeonidasTs@users.noreply.github.com>
2025-10-31 23:43:50 +0000

    Deploying to gh-pages from @ LeonidasTs/LeonidasTs.github.io@426fa5afd800f943f3eced8447d59afc61627537 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,10 +52,7 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "news-announcement-add-paper-here",
-          title: '📢 **Announcement: Add_paper_here 🎉🎉**',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+      },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
